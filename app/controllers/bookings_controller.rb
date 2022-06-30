84bf3294302d7b60e717986cf0318b296fd36d7c
Raphael Marsan <marsanraphael@gmail.com>
2022-07-01 01:39:43 +0200

    basic form for booking creation

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,21 @@
+class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params.except(:prestation_ids))
+    @booking.prestation_ids = Prestation.where(reference: params[:booking][:prestation_ids].reject(&:blank?)).pluck(:id)
+    redirect_to booking_path(@booking) if @booking.save!
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:email, :name, :starts_at, :address, prestation_references: [])
+  end
+end
