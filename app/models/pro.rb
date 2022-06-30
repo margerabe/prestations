0c723677033706f60a345a17489d3f4777f7002d
Raphael Marsan <marsanraphael@gmail.com>
2022-06-30 20:39:25 +0200

    add dependent destroy to pro associations

--- app/models/pro.rb
+++ app/models/pro.rb
@@ -1,6 +1,6 @@
 class Pro < ApplicationRecord
   has_many :pro_prestations
   has_many :prestations, through: :pro_prestations
-  has_many :appointments
-  has_many :opening_hours
+  has_many :appointments, dependent: :destroy
+  has_many :opening_hours, dependent: :destroy
 end
