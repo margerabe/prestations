b7070e39666de741c3a1ce866800b36fb41dbd94
Raphael Marsan <marsanraphael@gmail.com>
2022-07-01 14:46:00 +0200

    add comment about polymorphic join

--- app/models/prestation.rb
+++ app/models/prestation.rb
@@ -3,4 +3,6 @@ class Prestation < ApplicationRecord
   has_many :pros, through: :pro_prestations
   has_many :booking_prestations
   has_many :bookings, through: :booking_prestations
+
+  # Potential improvement: create a polymorphic join table instead since both pros and bookings can have prestations
 end
